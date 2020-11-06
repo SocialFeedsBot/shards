7a27604ffd8e0aff6388e895ef5bbada7c2e6f3c
jacobbxnd <jacobpbond4@gmail.com>
2020-11-06 17:08:37 +0000

    Allow creating webhooks in GUILD_NEWS channels

--- src/resolvers/channel.js
+++ src/resolvers/channel.js
@@ -2,7 +2,7 @@ module.exports = async (client, message, input) => {
   if (input.match(/<#(\d{17,21})>/)) input = input.match(/<#(\d{17,21})>/)[1];
 
   const found = message.channel.guild.channels
-    .filter(ch => ch.type === 0)
+    .filter(ch => [0, 5].includes(ch.type))
     .filter(ch => input === ch.id ||
         input.toLowerCase() === ch.name.toLowerCase() ||
         ch.name.toLowerCase().startsWith(input.toLowerCase()) ||
