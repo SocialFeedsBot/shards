3a06bd74a914687d9e9dbfcb9f2fc2ed687e1ad6
jacob <jacobpbond4@gmail.com>
2020-12-09 14:47:07 +0000

    Fix restarting a bit

--- src/commands/admin/restart.js
+++ src/commands/admin/restart.js
@@ -4,13 +4,13 @@ module.exports = class extends Command {
 
   constructor(...args) {
     super(...args, {
-      args: [{ type: 'text', label: 'target', optional: false }, { type: 'int', label: 'id', optional: true }], hidden: true
+      args: [{ type: 'text', label: 'target', optional: false }, { type: 'text', label: 'id', optional: true }], hidden: true
     });
   }
 
   async run({ reply, client, args: { target, id } }) {
-    await reply(`Restarting services with type **${target}**`);
-    client.gatewayClient.restart({ name: target, id: id === 'all' ? 'all' : ([id] || 'all') });
+    await reply(`Restarting services with type **${target}** (ids: \`${id ? id.split(',').join('` `') : 'N/A'}\`)`);
+    client.gatewayClient.restart({ name: target, id: id ? (id === 'all' ? id : id.split(',')) : undefined });
   }
 
 };
