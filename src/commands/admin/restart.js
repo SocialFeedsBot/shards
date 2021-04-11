85c290da53abbcf7b7113efb360c08513658ddf3
jacob <jacobpbond4@gmail.com>
2021-04-11 01:05:24 +0100

    visual

--- src/commands/admin/restart.js
+++ src/commands/admin/restart.js
@@ -9,7 +9,7 @@ module.exports = class extends Command {
   }
 
   async run({ reply, author, client, args: { target, id } }) {
-    await reply(`Restarting services with type **${target}** (ids: \`${id ? id.split(',').join('` `') : 'N/A'}\`)`);
+    await reply(`Restarting services with type **${target}** (ids: \`${id ? id.split(',').join('` `') : 'all'}\`)`);
     client.gatewayClient.action('restart', { name: target, id: id ? (id === 'all' ? id : id.split(',')) : undefined }, { restarter: author.id, panel: false });
   }
 
