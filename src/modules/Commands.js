7581fddbe8d01b418269fb909dd0911f65700bac
Jacob Bond <jacobpbond4@gmail.com>
2021-08-31 16:02:33 +0100

    feat: disable commands

--- src/modules/Commands.js
+++ src/modules/Commands.js
@@ -31,21 +31,11 @@ module.exports = class CommandsModule extends Collection {
 
     try {
       await message.reply([
-        ':warning: **Commands starting with the `sf!` prefix will no longer work soon.**',
+        ':x: **Commands starting with the `sf!` prefix have been disabled.**',
         'Discord has released slash commands which are a lot more easier to use and more recommended.',
-        'Try using a slash command by typing `/` and selecting the SocialFeeds logo to see commands.'
+        'Try using a slash command by typing `/` and selecting the SocialFeeds logo to see commands.',
+        'If you do not see any commands, kick the bot and invite it using this link: <https://socialfeeds.app/invite>'
       ].join('\n'));
-      await command.run({
-        message: message,
-        reply: message.reply,
-        args: args,
-        channel: message.channel,
-        guild: message.channel.guild,
-        author: message.author,
-        member: message.member,
-        client: this.client,
-        resolve: (type, input) => require(`../resolvers/${type}`)(this.client, message, input)
-      });
     } catch (err) {
       message.reply(`An error occurred, please try again later or report this error to our support server.\n${err.message}`, { success: false });
       this.client.logger.error(err.stack);
