a832334e0e023ae94891621bb81b302416eb2a59
Jacob Bond <jacobpbond4@gmail.com>
2022-04-25 22:50:01 +0100

    feat: add stats

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/SocialFeedsBot/shards/gateway"
 	"github.com/SocialFeedsBot/shards/internal/logger"
@@ -42,6 +47,10 @@ func main() {
 		return
 	}
 
+	if os.Getenv("BETA") != "yes" {
+		go startStatInterval(manager)
+	}
+
 	// Keep the process running until kill signals
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
@@ -50,4 +59,55 @@ func main() {
 	// Cleanly close the shards
 	logrus.Info("Stopping running shards")
 	session.ShardManager.StopAll()
+}
+
+func startStatInterval(manager *shardmanager.Manager) {
+	/*
+
+		req, err := http.NewRequest("GET", "http://example.com", nil)
+		// ...
+		req.Header.Add("If-None-Match", `W/"wyzzy"`)
+		resp, err := client.Do(req)
+	*/
+
+	client := &http.Client{}
+
+	ticker := time.NewTicker(60 * time.Second)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				guildCount := manager.GetFullStatus().NumGuilds
+
+				// prom
+				re, _ := http.NewRequest("POST", fmt.Sprintf("%v/gauge/set/guilds/%v", os.Getenv("PROMETHEUS_URL"), guildCount), bytes.NewBufferString(""))
+				client.Do(re)
+
+				// top.gg
+				topggValues := map[string]string{"server_count": fmt.Sprintf("%v", guildCount)}
+				topggData, _ := json.Marshal(topggValues)
+				topgg, _ := http.NewRequest("POST", fmt.Sprintf("https://top.gg/api/bots/%v/stats", os.Getenv("CLIENT_ID")), bytes.NewBuffer(topggData))
+				topgg.Header.Add("Authorization", os.Getenv("STATS_TOPGG"))
+				client.Do(topgg)
+
+				// dbl.com
+				dblValues := map[string]string{"guildCount": fmt.Sprintf("%v", guildCount)}
+				dblData, _ := json.Marshal(dblValues)
+				dbl, _ := http.NewRequest("POST", fmt.Sprintf("https://discord.bots.gg/api/v1/bots/%v/stats", os.Getenv("CLIENT_ID")), bytes.NewBuffer(dblData))
+				dbl.Header.Add("Authorization", os.Getenv("STATS_DBL"))
+				client.Do(dbl)
+
+				// discord.bots.gg
+				dbotsValues := map[string]string{"guildCount": fmt.Sprintf("%v", guildCount)}
+				dbotsData, _ := json.Marshal(dbotsValues)
+				dbots, _ := http.NewRequest("POST", fmt.Sprintf("https://discord.bots.gg/api/v1/bots/%v/stats", os.Getenv("CLIENT_ID")), bytes.NewBuffer(dbotsData))
+				dbots.Header.Add("Authorization", os.Getenv("STATS_DBOTS"))
+				client.Do(dbots)
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
 }
